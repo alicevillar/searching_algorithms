596c2816a854dd8cc28a6545fce4da94b2f35cae
Alice Villar <alsaldanhavillar@gmail.com>
2021-06-13 12:48:53 -0300

    update

--- linear_search/linear_search.py
+++ linear_search/linear_search.py
@@ -1,6 +1,40 @@
 
 
-# SIMPLE VERSION:
+# Python3 code to linearly search x in arr[].
+# If x is present then return its location,
+# otherwise return -1
+
+#Start from the leftmost element of arr[] and one by one compare x with each element of arr[]
+#If x matches with an element, return the index.
+#If x doesn’t match with any of elements, return -1.
+
+
+# Driver Code
+array = [2, 3, 4, 10, 40]
+size = len(array)
+target = 10
+
+def searching(array, size, target): # 3 parameters: the array, size of the array, target number
+    for i in range(size):
+        if (array[i] == target):
+            return i # returns the index
+    return -1 # didn't found the number
+
+# Calling the function and printing the result
+result = searching(array, size, target)
+if result == -1:
+    print("Element is not present in array")
+else:
+    print("Element is present at index", result)
+
+    #Output = Element is present at index 3
+
+    # ANOTHER VERSION
+
+
+'''   
+# ALTERNATIVE VERSION:
+# aqui vai achar todas as ocorrencias, etnao usar para o caso de um n q repete na lista
 
 def search(list,n):
     for i in range(len(list)):
@@ -10,38 +44,4 @@ def search(list,n):
 list = [1,2,3,4,5,6,7,8,9,0]
 n = 7
 search(list,n)
-
-# ANOTHER VERSION:
-
-# Python3 code to linearly search x in arr[].
-# If x is present then return its location,
-# otherwise return -1
-
-#Start from the leftmost element of arr[] and one by one compare x with each element of arr[]
-#If x matches with an element, return the index.
-#If x doesn’t match with any of elements, return -1.
-
-def search(arr, n, x):
-    for i in range(0, n):
-        if (arr[i] == x):
-            return i
-    return -1
-
-
-# Driver Code
-arr = [2, 3, 4, 10, 40]
-x = 10
-n = len(arr)
-
-# Function call
-result = search(arr, n, x)
-if (result == -1):
-    print("Element is not present in array")
-else:
-    print("Element is present at index", result)
-
-    #Output = Element is present at index 3
-    # https://www.geeksforgeeks.org/linear-search/
-
-    # ANOTHER VERSION
-
+'''
--- readme.md
+++ readme.md
@@ -13,4 +13,26 @@
 10. Recursive function to do substring search
 11. Unbounded Binary Search Example (Find the point where a monotonically increasing function becomes positive first time)
 
+## Linear Search Algorithm  
+
+
+Search is a process of finding a value in a list of values. In other words, searching is the process of locating given value position in a list of values.
+
+Linear search algorithm finds a given element in a list of elements with O(n) time complexity where n is total number of elements in the list. This search process starts comparing search element with the first element in the list. If both are matched then result is element found otherwise search element is compared with the next element in the list. Repeat the same until search element is compared with the last element in the list, if that last element also doesn't match, then the result is "Element not found in the list". That means, the search element is compared with element by element in the list.
+
+Linear search is implemented using following steps...
+•	Step 1 - Read the search element from the user.
+•	Step 2 - Compare the search element with the first element in the list.
+•	Step 3 - If both are matched, then display "Given element is found!!!" and terminate the function
+•	Step 4 - If both are not matched, then compare search element with the next element in the list.
+•	Step 5 - Repeat steps 3 and 4 until search element is compared with last element in the list.
+•	Step 6 - If last element in the list also doesn't match, then display "Element is not found!!!" and terminate the function.
+
+
+
+
+
+
+
+
 https://www.geeksforgeeks.org/linear-search/
