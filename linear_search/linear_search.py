36a1cc55133354f17e381a2fae778133c1157084
Alice Villar <alsaldanhavillar@gmail.com>
2021-06-13 13:25:21 -0300

    linear search (v1 & v2)

--- linear_search/linear_search.py
+++ linear_search/linear_search.py
@@ -1,23 +1,22 @@
 
-
-# Python3 code to linearly search x in arr[].
-# If x is present then return its location,
-# otherwise return -1
-
-#Start from the leftmost element of arr[] and one by one compare x with each element of arr[]
-#If x matches with an element, return the index.
-#If x doesn’t match with any of elements, return -1.
+##############################################################################################################
+#
+#   Linear Search - VERSION 1
+#   This function compare the target number with each element of the array
+#   If the target number is present, then return its location
+#
+##############################################################################################################
 
 
 # Driver Code
-array = [2, 3, 4, 10, 40]
+array = [2, 3, 4, 40, 70, 10, 84, 59, 33]
 size = len(array)
 target = 10
 
 def searching(array, size, target): # 3 parameters: the array, size of the array, target number
-    for i in range(size):
-        if (array[i] == target):
-            return i # returns the index
+    for i in range(size): # One by one compare the target number with each element of the array
+        if (array[i] == target): # check if the target number matches an element in the list
+            return i # If the target number is present then return its location
     return -1 # didn't found the number
 
 # Calling the function and printing the result
@@ -27,21 +26,30 @@ if result == -1:
 else:
     print("Element is present at index", result)
 
-    #Output = Element is present at index 3
 
-    # ANOTHER VERSION
+# OUTPUT =>> Element is present at index 5
 
-
-'''   
-# ALTERNATIVE VERSION:
-# aqui vai achar todas as ocorrencias, etnao usar para o caso de um n q repete na lista
-
-def search(list,n):
-    for i in range(len(list)):
-        if list[i] == n:
-            print(i)
+##############################################################################################################
+#
+#   Linear Search - VERSION 2
+#   If the target element is present in the list more than once, this function will return the locations
+#
+##############################################################################################################
+  
+ 
+ 
+def search(list,target):
+    for i in range(len(list)): # One by one compare the target number with each element of the array
+        if list[i] == target: # check if the target number matches an element in the list
+            print(i) # If the target number is present then return all the locations
     return -1
-list = [1,2,3,4,5,6,7,8,9,0]
-n = 7
-search(list,n)
-'''
+
+
+list = [1,2,7,4,5,6,7,8,9,7]
+target = 7
+search(list,target)
+
+# OUTPUT =>> 
+# 2
+# 6
+# 9
